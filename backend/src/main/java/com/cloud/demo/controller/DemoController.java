8e14ba8302076ed09ca6cf648993c0eb9cd6e804
LiYi <li.yi@cloud-ace.jp>
2020-05-29 11:04:50 +0900

    コントローラーのサンプル

---
+++ backend/src/main/java/com/cloud/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.cloud.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String HelloCloud() {
+        return "Hello Cloud !";
+    }
+}
