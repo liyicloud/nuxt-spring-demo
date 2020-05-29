2b5e28fd2cf10a94d151fe05fb4b0e6d525a8923
LiYi <li.yi@cloud-ace.jp>
2020-05-29 11:16:41 +0900

    https://qiita.com/kazuki43zoo/items/757b557c05f548c6c5db リクエスト共通処理のサンプル

---
+++ backend/src/main/java/com/cloud/demo/infrastructure/listener/CustomListener.java
@@ -0,0 +1,28 @@
+package com.cloud.demo.infrastructure.listener;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomListener implements ServletRequestListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomListener.class);
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        logger.info("******* RequestInitialized : {}" , sre);
+        // リクエスト開始時の処理を行う。
+        // (実装は省略)
+    }
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        logger.info("******* RequestDestroyed : {}" , sre);
+        // リクエスト終了時の処理を行う。
+        // (実装は省略)
+    }
+
+}
